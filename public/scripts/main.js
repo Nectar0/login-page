401349ac9cc9bda25eb2d730023f69245b8d68a4
Alex Strugacz <alexstrugacz@icloud.com>
2022-09-17 11:38:56 -0500

    firebase work

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,1 @@
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    //user is signed in
-    document.getElementById("user-div").style.display = "block";
-    document.getElementById("login-div").style.display = "none";
-  } else {
-    //user is not signed in
-
-    document.getElementById("login-div").style.display = "block";
-    document.getElementById("user-div").style.display = "none";
-  }
-});
+// js
